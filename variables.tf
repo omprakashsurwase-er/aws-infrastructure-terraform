7fddd260931d9d48b12a2be37b72fa223d14e6d3
omprakashsurwase-er <omprakashsurwase1508@gmail.com>
2026-04-13 10:43:14 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,83 +1,47 @@
-provider "aws" {
-  region = var.aws_region
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
 }
 
-# VPC
-resource "aws_vpc" "main" {
-  cidr_block = var.vpc_cidr
-
-  tags = {
-    Name = "${var.project_name}-vpc"
-  }
+variable "project_name" {
+  description = "Project name"
+  type        = string
+  default     = "myapp"
 }
 
-# Public Subnet 1
-resource "aws_subnet" "public_1" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = var.public_subnet_1_cidr
-  availability_zone       = "${var.aws_region}a"
-  map_public_ip_on_launch = true
+variable "vpc_cidr" {
+  description = "VPC CIDR block"
+  type        = string
+  default     = "10.0.0.0/16"
 }
 
-# Public Subnet 2
-resource "aws_subnet" "public_2" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = var.public_subnet_2_cidr
-  availability_zone       = "${var.aws_region}b"
-  map_public_ip_on_launch = true
+variable "public_subnet_1_cidr" {
+  description = "Public subnet 1 CIDR block"
+  type        = string
+  default     = "10.0.1.0/24"
 }
 
-# Internet Gateway
-resource "aws_internet_gateway" "main" {
-  vpc_id = aws_vpc.main.id
+variable "public_subnet_2_cidr" {
+  description = "Public subnet 2 CIDR block"
+  type        = string
+  default     = "10.0.2.0/24"
 }
 
-# Security Group
-resource "aws_security_group" "main" {
-  name   = "${var.project_name}-sg"
-  vpc_id = aws_vpc.main.id
-
-  ingress {
-    from_port   = var.app_port
-    to_port     = var.app_port
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = [var.ssh_cidr]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
 }
 
-# EC2 Instance
-resource "aws_instance" "app_server" {
-  ami           = "ami-0c02fb55956c7d316"
-  instance_type = var.instance_type
-  subnet_id     = aws_subnet.public_1.id
-  vpc_security_group_ids = [aws_security_group.main.id]
-
-  tags = {
-    Name = "${var.project_name}-instance"
-  }
+variable "app_port" {
+  description = "Application port"
+  type        = number
+  default     = 80
 }
 
-# Load Balancer
-resource "aws_lb" "main" {
-  name               = "${var.project_name}-alb"
-  load_balancer_type = "application"
-  subnets            = [
-    aws_subnet.public_1.id,
-    aws_subnet.public_2.id
-  ]
-  security_groups    = [aws_security_group.main.id]
+variable "ssh_cidr" {
+  description = "CIDR block for SSH"
+  type        = string
+  default     = "0.0.0.0/0"
 }
